6c5a52c15b02c4d256b5a7516a4a82b8054ef51d
Gheorghe <georgebucu96@gmail.com>
2020-12-07 13:31:40 +0100

    Añade atributos en clase modelo

---
+++ src/main/java/daw/gheorghe/Moto.java
@@ -0,0 +1,13 @@
+package daw.gheorghe;
+
+/**
+ *
+ * @author Jorge
+ */
+public class Moto {
+    
+    private String modelo;
+    private String color;
+    private String motor;
+    private String marca;
+}
