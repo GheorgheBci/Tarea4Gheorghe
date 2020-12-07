1fa904296b7a77558af49753891e6230821ca0c7
Alberto <albertogr578@gmail.com>
2020-12-07 13:59:38 +0100

    Añade métodos getters y setters y metodo toString

--- src/main/java/daw/gheorghe/Moto.java
+++ src/main/java/daw/gheorghe/Moto.java
@@ -24,5 +24,44 @@ public class Moto {
         this.color = "Rojo";
         this.motor = "142.0 cv";
         this.marca = "Kawasaki";
+
     }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getMotor() {
+        return motor;
+    }
+
+    public void setMotor(String motor) {
+        this.motor = motor;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    @Override
+    public String toString() {
+        return "Moto{" + "modelo=" + modelo + ", color=" + color + ", motor=" + motor + ", marca=" + marca + '}';
+    }
+
 }
