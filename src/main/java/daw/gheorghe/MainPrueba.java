19d7d3a44e75a53b4e8b0769b6290b49350f3d64
Gheorghe <georgebucu96@gmail.com>
2020-12-07 14:11:24 +0100

    Añade objetos en la clase main e imprime sus valores

--- src/main/java/daw/gheorghe/MainPrueba.java
+++ src/main/java/daw/gheorghe/MainPrueba.java
@@ -5,8 +5,15 @@ package daw.gheorghe;
  * @author Jorge
  */
 public class MainPrueba {
-    
+
     public static void main(String[] args) {
+
+        Moto moto = new Moto("GSX-S125", "negro", "142.0 cv", "Suzuki");
+
+        Moto moto2 = new Moto();
+
+        System.out.println("Datos de la moto: " + moto.getMarca() + ", " + moto.getModelo() + ", " + moto.getMotor() + ", " + moto.getColor());
         
+        System.out.println("Datosd de la moto2: " + moto2);
     }
 }
