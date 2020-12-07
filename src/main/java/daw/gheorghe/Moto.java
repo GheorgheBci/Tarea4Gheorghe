0d5de6f4370791bd2a81a5070310ecf4cc56a492
Alberto <albertogr578@gmail.com>
2020-12-07 13:51:57 +0100

    Añade constructores en la clase Moto

--- src/main/java/daw/gheorghe/Moto.java
+++ src/main/java/daw/gheorghe/Moto.java
@@ -5,9 +5,24 @@ package daw.gheorghe;
  * @author Jorge
  */
 public class Moto {
-    
+
     private String modelo;
     private String color;
     private String motor;
     private String marca;
+
+    public Moto(String modelo, String color, String motor, String marca) {
+        this.modelo = modelo;
+        this.color = color;
+        this.motor = motor;
+        this.marca = marca;
+    }
+
+    public Moto() {
+
+        this.modelo = "Ninja 1000SX 2020";
+        this.color = "Rojo";
+        this.motor = "142.0 cv";
+        this.marca = "Kawasaki";
+    }
 }
